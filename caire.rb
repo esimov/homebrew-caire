926e967924dddc6a11b909d79b21d271b7a74300
Endre Simo <esimovaq@yahoo.com>
2018-06-12 17:47:05 +0300

    Update homebrew version

--- caire.rb
+++ caire.rb
@@ -1,10 +1,10 @@
-CAIRE_VERSION="1.0.2"
+CAIRE_VERSION="1.1.0"
 
 class Caire < Formula
   desc "Content aware image resize library"
   homepage "https://github.com/esimov/caire"
   url "#{homepage}/releases/download/v#{CAIRE_VERSION}/caire-#{CAIRE_VERSION}-darwin-amd64.zip"
-  sha256 "ccd9f187d83873ede55240629e45bc7380ba6bcd747ef86e5cbae11268844319"
+  sha256 "52ff0b6b86b66a325233b4c627d8d0e922d74e6904bc1f41605f41006e91007f"
 
   depends_on "go" => :build
 
